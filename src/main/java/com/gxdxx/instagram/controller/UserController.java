893c22d754a88775db9cf41c796325ca93fb2ef8
gxdxx-new <nkd0310@naver.com>
2023-04-03 20:19:32 +0900

    #1 - feat: 회원 탈퇴 기능 구현
    DB에서 요청으로 받은 id값을 가지는 회원이 있는지 조회 후 soft delete로 회원 탈퇴 처리를 한다.
    추후 스프링 시큐리티 토큰 인증 기능을 사용할 것이기 때문에 요청한 회원과 탈퇴 시킬 회원이 같은지 확인하는 로직을 추가해야 한다.

--- src/main/java/com/gxdxx/instagram/controller/UserController.java
+++ src/main/java/com/gxdxx/instagram/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.gxdxx.instagram.controller;
 
 import com.gxdxx.instagram.dto.request.UserSignUpRequest;
+import com.gxdxx.instagram.dto.response.SuccessResponse;
 import com.gxdxx.instagram.dto.response.UserSignUpResponse;
 import com.gxdxx.instagram.exception.InvalidRequestException;
 import com.gxdxx.instagram.service.UserService;
@@ -18,13 +19,19 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserSignUpResponse registerMember(
+    public UserSignUpResponse registerUser(
             @Valid UserSignUpRequest request, BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) {
             throw new InvalidRequestException();
         }
         return userService.saveUser(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public SuccessResponse deleteUser(@PathVariable("id") Long id) {
+        // TODO: JWT 토큰의 ID값으로 유저가 맞는지 확인
+        return userService.deleteUser(id);
     }
 
 }
--- src/main/java/com/gxdxx/instagram/service/UserService.java
+++ src/main/java/com/gxdxx/instagram/service/UserService.java
@@ -1,9 +1,11 @@
 package com.gxdxx.instagram.service;
 
 import com.gxdxx.instagram.dto.request.UserSignUpRequest;
+import com.gxdxx.instagram.dto.response.SuccessResponse;
 import com.gxdxx.instagram.dto.response.UserSignUpResponse;
 import com.gxdxx.instagram.entity.User;
 import com.gxdxx.instagram.exception.NicknameAlreadyExistsException;
+import com.gxdxx.instagram.exception.UserNotFoundException;
 import com.gxdxx.instagram.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +31,13 @@ public class UserService {
 
         User saveUser = User.of(request.nickname(), profileImageUrl);
         return UserSignUpResponse.of(userRepository.save(saveUser));
+    }
+
+    public SuccessResponse deleteUser(Long id) {
+        // TODO: 본인이 맞는지 확인
+        User deleteUser = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        userRepository.delete(deleteUser);
+        return SuccessResponse.of("회원탈퇴를 성공했습니다.");
     }
 
 }
