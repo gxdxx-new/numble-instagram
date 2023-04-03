7a7831b1fcd3dabbc21b5a020906ca763dcd49ae
gxdxx-new <nkd0310@naver.com>
2023-04-03 19:31:45 +0900

    #1 - feat: 회원가입 기능 구현
    Controller에서 유효성 검사를 통과한 경우 Service에서 중복된 닉네임인지 확인 후 회원을 저장한다.
    현재 프로필 이미지 업로드 기능은 구현하지 않았고 추후에 S3에 업로드하는 기능을 구현할 예정이다.

--- src/main/java/com/gxdxx/instagram/controller/UserController.java
+++ src/main/java/com/gxdxx/instagram/controller/UserController.java
@@ -1,12 +1,30 @@
 package com.gxdxx.instagram.controller;
 
+import com.gxdxx.instagram.dto.request.UserSignUpRequest;
+import com.gxdxx.instagram.dto.response.UserSignUpResponse;
+import com.gxdxx.instagram.exception.InvalidRequestException;
+import com.gxdxx.instagram.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/users/")
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserSignUpResponse registerMember(
+            @Valid UserSignUpRequest request, BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestException();
+        }
+        return userService.saveUser(request);
+    }
+
 }
--- src/main/java/com/gxdxx/instagram/service/UserService.java
+++ src/main/java/com/gxdxx/instagram/service/UserService.java
@@ -1,5 +1,9 @@
 package com.gxdxx.instagram.service;
 
+import com.gxdxx.instagram.dto.request.UserSignUpRequest;
+import com.gxdxx.instagram.dto.response.UserSignUpResponse;
+import com.gxdxx.instagram.entity.User;
+import com.gxdxx.instagram.exception.NicknameAlreadyExistsException;
 import com.gxdxx.instagram.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -11,5 +15,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserSignUpResponse saveUser(UserSignUpRequest request) {
+        userRepository.findByNickname(request.nickname()).ifPresent(user -> {
+            throw new NicknameAlreadyExistsException();
+        });
+        
+        // TODO: 파일업로드 구현하기
+        String profileImageUrl = new StringBuffer()
+                .append("http://example.com/images/.jpg")
+                .insert(26, request.nickname())
+                .toString();
+
+        User saveUser = User.of(request.nickname(), profileImageUrl);
+        return UserSignUpResponse.of(userRepository.save(saveUser));
+    }
 
 }
