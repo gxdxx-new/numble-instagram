55933b1ee21252d95dad9b94b824787525760a1b
gxdxx-new <nkd0310@naver.com>
2023-04-06 19:31:14 +0900

    #5 - feat: Follow 엔티티 설계
    Follow 엔티티에는 follower와 following이 있다.
    following은 follower를 follow 하는 사람이고,
    follower는 following에게 follow를 받는 사람이다.

---
+++ src/main/java/com/gxdxx/instagram/entity/Follow.java
@@ -0,0 +1,51 @@
+package com.gxdxx.instagram.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User following;
+
+    private Follow(User follower, User following) {
+        this.follower = follower;
+        this.following = following;
+    }
+
+    public static Follow of(User follower, User following) {
+        return new Follow(follower, following);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Follow follow)) return false;
+        return id != null && id.equals(follow.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
