42862f3e40153c77da9651c4aaab132729cee93f
gxdxx-new <nkd0310@naver.com>
2023-04-07 18:25:40 +0900

    #9 - feat: 팔로우 요청 취소 비즈니스 로직 구현
    1. 팔로워, 팔로잉 회원을 DB에서 조회한다. 존재하지 않으면 "UserNotFoundException()" 예외를 발생시킨다.
    2. "findByFollowerAndFollowing()" 쿼리 메소드를 이용해 팔로워 회원과 팔로잉 회원이 팔로우 관계로 되어있는지 확인한다. 팔로우 관계가 아닐 경우 "FollowNotFoundException()" 예외를 발생시킨다.
    3. 팔로우 관계를 취소시킨다. 이 때 soft delete를 적용해 "Follow" 엔티티의 "deleted" 필드를 true로 변경한다.

--- src/main/java/com/gxdxx/instagram/service/FollowService.java
+++ src/main/java/com/gxdxx/instagram/service/FollowService.java
@@ -5,6 +5,7 @@ import com.gxdxx.instagram.dto.response.SuccessResponse;
 import com.gxdxx.instagram.entity.Follow;
 import com.gxdxx.instagram.entity.User;
 import com.gxdxx.instagram.exception.FollowAlreadyExistsException;
+import com.gxdxx.instagram.exception.FollowNotFountException;
 import com.gxdxx.instagram.exception.InvalidRequestException;
 import com.gxdxx.instagram.exception.UserNotFoundException;
 import com.gxdxx.instagram.repository.FollowRepository;
@@ -37,6 +38,19 @@ public class FollowService {
         }
 
         followRepository.save(Follow.createFollow(follower, following));
+        return SuccessResponse.of("200 SUCCESS");
+    }
+
+    public SuccessResponse deleteFollow(Long followingId, String followerNickname) {
+
+        User follower = userRepository.findByNickname(followerNickname)
+                .orElseThrow(UserNotFoundException::new);
+        User following = userRepository.findById(followingId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Follow follow = followRepository.findByFollowerAndFollowing(follower, following)
+                .orElseThrow(FollowNotFountException::new);
+        followRepository.delete(follow);
         return SuccessResponse.of("200 SUCCESS");
     }
 
