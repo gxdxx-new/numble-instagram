2c66e8f0f11fc7ab85e7e58ad23a10015c67fa3c
gxdxx-new <nkd0310@naver.com>
2023-04-07 18:15:37 +0900

    #9 - feat: findByFollowerAndFollowing() 쿼리 메소드 작성
    "follower"가 "following"을 팔로우하고 있는지 확인하는 쿼리 메소드이다.
     "follower"와 "following" 간의 "Follow" 관계가 없는 경우, Optional.empty()를 반환한다.

--- src/main/java/com/gxdxx/instagram/repository/FollowRepository.java
+++ src/main/java/com/gxdxx/instagram/repository/FollowRepository.java
@@ -4,8 +4,12 @@ import com.gxdxx.instagram.entity.Follow;
 import com.gxdxx.instagram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFollowerAndFollowing(User follower, User following);
+
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 
 }
