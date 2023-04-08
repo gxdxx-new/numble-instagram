4962cb0a80bd8f56c4fc8f8e7de3cf2998b22a5f
gxdxx-new <nkd0310@naver.com>
2023-04-08 19:12:31 +0900

    #13 - feat: 프로필 수정 요청 객체 수정
    프로필 이미지의 경우 필수로 전달받도록 수정

--- src/main/java/com/gxdxx/instagram/dto/request/UserProfileUpdateRequest.java
+++ src/main/java/com/gxdxx/instagram/dto/request/UserProfileUpdateRequest.java
@@ -2,10 +2,11 @@ package com.gxdxx.instagram.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UserProfileUpdateRequest(
         @JsonProperty("nickname") @NotBlank String nickname,
-        @JsonProperty("profile_image") MultipartFile profileImage
+        @JsonProperty("profile_image") @NotNull MultipartFile profileImage
 ) {
 }
