9b8cea3a4d3a4d84ee74396eefc68f7805048185
gxdxx-new <nkd0310@naver.com>
2023-04-16 20:39:25 +0900

    #21 - feat: 게시물 등록 기능 구현

--- src/main/java/com/gxdxx/instagram/controller/PostController.java
+++ src/main/java/com/gxdxx/instagram/controller/PostController.java
@@ -1,9 +1,17 @@
 package com.gxdxx.instagram.controller;
 
+import com.gxdxx.instagram.dto.request.PostRegisterRequest;
+import com.gxdxx.instagram.dto.response.PostRegisterResponse;
+import com.gxdxx.instagram.exception.InvalidRequestException;
 import com.gxdxx.instagram.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
@@ -11,5 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping
+    public PostRegisterResponse registerPost(
+            PostRegisterRequest request,
+            BindingResult bindingResult,
+            Principal principal
+    ) throws IOException {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestException();
+        }
+        return postService.registerPost(request, principal.getName());
+    }
 
 }
--- src/main/java/com/gxdxx/instagram/service/PostService.java
+++ src/main/java/com/gxdxx/instagram/service/PostService.java
@@ -1,9 +1,17 @@
 package com.gxdxx.instagram.service;
 
+import com.gxdxx.instagram.dto.request.PostRegisterRequest;
+import com.gxdxx.instagram.dto.response.PostRegisterResponse;
+import com.gxdxx.instagram.entity.Post;
+import com.gxdxx.instagram.entity.User;
+import com.gxdxx.instagram.exception.UserNotFoundException;
 import com.gxdxx.instagram.repository.PostRepository;
+import com.gxdxx.instagram.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Transactional
@@ -11,5 +19,14 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
+
+    public PostRegisterResponse registerPost(PostRegisterRequest request, String userNickname) throws IOException {
+        User user = userRepository.findByNickname(userNickname).orElseThrow(UserNotFoundException::new);
+        String imageUrl = s3Uploader.upload(request.image(), "images");
+        Post post = Post.of(request.content(), imageUrl, user);
+        return PostRegisterResponse.of(postRepository.save(post));
+    }
 
 }
