35a8a3a653be5ae960e3045b2f517fa0ec6679fd
gxdxx-new <nkd0310@naver.com>
2023-04-29 01:23:44 +0900

    #31 - feat: 메시지 엔티티 설계

---
+++ src/main/java/com/gxdxx/instagram/entity/Message.java
@@ -0,0 +1,64 @@
+package com.gxdxx.instagram.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE message SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private boolean deleted = Boolean.FALSE;
+
+    private Message(String content, ChatRoom chatRoom, User sender, User receiver) {
+        this.content = content;
+        this.chatRoom = chatRoom;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public static Message of(String content, ChatRoom chatRoom, User sender, User receiver) {
+        return new Message(content, chatRoom, sender, receiver);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message message)) return false;
+        return id != null && id.equals(message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
