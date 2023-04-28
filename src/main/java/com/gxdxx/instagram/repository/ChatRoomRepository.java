b93613f2b9a9cfd7c78050f3f4ea9d6d326a33d7
gxdxx-new <nkd0310@naver.com>
2023-04-28 21:19:32 +0900

    #29 - feat: 채팅방 Controller, Service, Repository 생성

---
+++ src/main/java/com/gxdxx/instagram/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.gxdxx.instagram.repository;
+
+import com.gxdxx.instagram.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
