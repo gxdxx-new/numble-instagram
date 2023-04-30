f9c1d41443cf979b6b53921d31f231747da3d617
gxdxx-new <nkd0310@naver.com>
2023-04-30 12:34:13 +0900

    #33 - feat: DM 전송 컨트롤러 생성

--- src/main/java/com/gxdxx/instagram/controller/MessageController.java
+++ src/main/java/com/gxdxx/instagram/controller/MessageController.java
@@ -1,9 +1,18 @@
 package com.gxdxx.instagram.controller;
 
+import com.gxdxx.instagram.dto.request.MessageSendRequest;
+import com.gxdxx.instagram.dto.response.SuccessResponse;
+import com.gxdxx.instagram.exception.InvalidRequestException;
 import com.gxdxx.instagram.service.MessageService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/messages")
@@ -11,5 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final MessageService messageService;
+
+    @PostMapping
+    public SuccessResponse sendMessage(
+            @RequestBody @Valid MessageSendRequest request,
+            BindingResult bindingResult,
+            Principal principal
+    ) {
+
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestException();
+        }
+
+        return messageService.sendMessage(request, principal.getName());
+    }
 
 }
