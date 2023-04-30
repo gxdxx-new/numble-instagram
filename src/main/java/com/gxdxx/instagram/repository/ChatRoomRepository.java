c85496d811f53ffc8968433b64e9d19378c761cb
gxdxx-new <nkd0310@naver.com>
2023-04-30 12:38:43 +0900

    #33 - feat: 채팅방 조회 JPQL 쿼리 작성
    현재 채팅방 컬럼에 순서 상관없이 단순히 두 회원의 id만 저장되어 있기 때문에 JPQL 쿼리에서 OR 절을 사용해 senderId와 receiverId를 각각 대입해보아야 한다.

--- src/main/java/com/gxdxx/instagram/repository/ChatRoomRepository.java
+++ src/main/java/com/gxdxx/instagram/repository/ChatRoomRepository.java
@@ -2,6 +2,16 @@ package com.gxdxx.instagram.repository;
 
 import com.gxdxx.instagram.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT cr FROM ChatRoom cr " +
+            "WHERE (cr.userA.id = :senderId AND cr.userB.id = :receiverId) " +
+            "OR (cr.userA.id = :receiverId AND cr.userB.id = :senderId)")
+    Optional<ChatRoom> findByUserIds(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
+
 }
