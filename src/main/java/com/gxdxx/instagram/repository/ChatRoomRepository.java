58996d00d58350c637f0506fa6aaf25d86b420bd
gxdxx-new <nkd0310@naver.com>
2023-05-02 14:04:39 +0900

    #35 - feat: 채팅방 최대 id값 반환 쿼리 작성
    cursor값이 null일 경우 가장 최근 채팅방에서부터 자기 자신이 속한 채팅방을 조회하기 위해 작성했다.

--- src/main/java/com/gxdxx/instagram/repository/ChatRoomRepository.java
+++ src/main/java/com/gxdxx/instagram/repository/ChatRoomRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
 
     @Query("SELECT cr FROM ChatRoom cr " +
             "WHERE (cr.userA.id = :senderId AND cr.userB.id = :receiverId) " +
             "OR (cr.userA.id = :receiverId AND cr.userB.id = :senderId)")
     Optional<ChatRoom> findByUserIds(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
+
+    @Query("SELECT MAX(cr.id) FROM ChatRoom cr")
+    Optional<Long> findMaxChatRoomId();
 
 }
