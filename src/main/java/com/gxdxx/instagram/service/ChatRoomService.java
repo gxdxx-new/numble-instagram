a6286b4cb2e5dfc178ed89424d22ec90d7d78278
gxdxx-new <nkd0310@naver.com>
2023-05-02 14:15:42 +0900

    #35 - feat: DM 목록 조회 비즈니스 로직 구현
    요청 cursor값이 null일 경우 가장 최근 채팅방부터 조회를 시작한다.
    
    채팅방을 조회 후 리스트가 비어있지 않다면, 다음 조회를 위해 다음 커서 값인 마지막 ChatRoomListResponse 객체의 chatRoomId 값을 응답에 전달한다.

--- src/main/java/com/gxdxx/instagram/service/ChatRoomService.java
+++ src/main/java/com/gxdxx/instagram/service/ChatRoomService.java
@@ -1,9 +1,18 @@
 package com.gxdxx.instagram.service;
 
+import com.gxdxx.instagram.dto.request.ChatRoomListRequest;
+import com.gxdxx.instagram.dto.response.ChatRoomListResponse;
+import com.gxdxx.instagram.entity.User;
+import com.gxdxx.instagram.exception.UserNotFoundException;
 import com.gxdxx.instagram.repository.ChatRoomRepository;
+import com.gxdxx.instagram.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Transactional
@@ -11,5 +20,21 @@ import org.springframework.stereotype.Service;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
+
+    public Map<String, Object> getChatRooms(ChatRoomListRequest request, String nickname) {
+        User user = userRepository.findByNickname(nickname)
+                .orElseThrow(UserNotFoundException::new);
+        Long cursor = (request.cursor() == null)
+                ? chatRoomRepository.findMaxChatRoomId().map(maxId -> maxId + 1).orElse(0L)
+                : request.cursor();
+        List<ChatRoomListResponse> list = chatRoomRepository.getChatRoomsByCursor(user.getId(), cursor, 5);
+        Long nextCursor = !list.isEmpty() ? list.get(list.size() - 1).getChatRoomId() : 0L;
+        Map<String, Object> response = new HashMap<>();
+        response.put("cursor", nextCursor);
+        response.put("chat_rooms", list);
+
+        return response;
+    }
 
 }
