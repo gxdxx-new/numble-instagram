0acd02c17887f5acd9ccc16d3b48b3c9bd2d2625
gxdxx-new <nkd0310@naver.com>
2023-05-03 13:21:26 +0900

    #37 - feat: DM 상세 내역 조회 컨트롤러 생성

--- src/main/java/com/gxdxx/instagram/controller/MessageController.java
+++ src/main/java/com/gxdxx/instagram/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.gxdxx.instagram.controller;
 
+import com.gxdxx.instagram.dto.request.MessageListRequest;
 import com.gxdxx.instagram.dto.request.MessageSendRequest;
 import com.gxdxx.instagram.dto.response.SuccessResponse;
 import com.gxdxx.instagram.exception.InvalidRequestException;
@@ -7,12 +8,10 @@ import com.gxdxx.instagram.service.MessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/messages")
@@ -33,6 +32,20 @@ public class MessageController {
         }
 
         return messageService.sendMessage(request, principal.getName());
+    }
+
+    @GetMapping("/{chatRoomId}")
+    public Map<String, Object> getMessages(
+            @RequestBody @Valid MessageListRequest request,
+            BindingResult bindingResult,
+            Principal principal
+    ) {
+
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRequestException();
+        }
+
+        return messageService.getMessages(request, principal.getName());
     }
 
 }
