2afb3ec7695e3d8392c9312c3c9ce12f9086ca1d
gxdxx-new <nkd0310@naver.com>
2023-05-03 13:22:24 +0900

    #37 - feat: 메시지 최대 id값 반환 쿼리 작성
    cursor값이 null일 경우 가장 최근 메시지에서부터 조회하기 위해 작성했다.

--- src/main/java/com/gxdxx/instagram/repository/MessageRepository.java
+++ src/main/java/com/gxdxx/instagram/repository/MessageRepository.java
@@ -2,6 +2,13 @@ package com.gxdxx.instagram.repository;
 
 import com.gxdxx.instagram.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
+import java.util.Optional;
+
+public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
+
+    @Query("SELECT MAX(m.id) FROM Message m")
+    Optional<Long> findMaxMessageId();
+
 }
