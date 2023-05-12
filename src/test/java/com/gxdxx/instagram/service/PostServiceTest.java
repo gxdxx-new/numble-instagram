d4742911c30eed2eb5cb3320043a17dfbef0f6b6
gxdxx-new <nkd0310@naver.com>
2023-05-12 15:09:23 +0900

    #47 - feat: 게시물 비즈니스 로직 단위 테스트 클래스 생성

---
+++ src/test/java/com/gxdxx/instagram/service/PostServiceTest.java
@@ -0,0 +1,25 @@
+package com.gxdxx.instagram.service;
+
+import com.gxdxx.instagram.repository.PostRepository;
+import com.gxdxx.instagram.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private S3Uploader s3Uploader;
+
+}
