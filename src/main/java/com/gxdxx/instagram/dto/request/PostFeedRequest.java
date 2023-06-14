6dde17a93c0f91845c6c487ef376d26e83bca6b7
gxdxx-new <nkd0310@naver.com>
2023-06-14 15:22:16 +0900

    #59 - feat: 게시글 API Swagger 적용

--- src/main/java/com/gxdxx/instagram/dto/request/PostFeedRequest.java
+++ src/main/java/com/gxdxx/instagram/dto/request/PostFeedRequest.java
@@ -1,12 +1,17 @@
 package com.gxdxx.instagram.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Positive;
 
 public record PostFeedRequest(
 
+        @Schema(description = "회원 id")
+        @JsonProperty("user_id")
         @Positive
         Long userId,
 
+        @Schema(description = "커서 (게시글 id)")
         Long cursor
 
 ) {
--- src/main/java/com/gxdxx/instagram/dto/response/PostUpdateResponse.java
+++ src/main/java/com/gxdxx/instagram/dto/response/PostUpdateResponse.java
@@ -2,11 +2,20 @@ package com.gxdxx.instagram.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gxdxx.instagram.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PostUpdateResponse(
-        @JsonProperty("id") Long id,
-        @JsonProperty("content") String content,
-        @JsonProperty("image_url") String imageUrl
+
+        @Schema(description = "게시글 id")
+        Long id,
+
+        @Schema(description = "게시글 내용")
+        String content,
+
+        @Schema(description = "사진 url")
+        @JsonProperty("image_url")
+        String imageUrl
+
 ) {
 
     public static PostUpdateResponse of(Post post) {
