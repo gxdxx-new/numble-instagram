bfb83ef7e733ca819adc48c83dfb8983c62835c6
gxdxx-new <nkd0310@naver.com>
2023-06-15 00:13:20 +0900

    #59 - feat: 답글 API Swagger 적용

--- src/main/java/com/gxdxx/instagram/dto/response/ReplyUpdateResponse.java
+++ src/main/java/com/gxdxx/instagram/dto/response/ReplyUpdateResponse.java
@@ -1,10 +1,16 @@
 package com.gxdxx.instagram.dto.response;
 
 import com.gxdxx.instagram.entity.Reply;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ReplyUpdateResponse(
+
+        @Schema(description = "답글 id")
         Long id,
+
+        @Schema(description = "답글 내용")
         String content
+
 ) {
 
     public static ReplyUpdateResponse of(Reply reply) {
