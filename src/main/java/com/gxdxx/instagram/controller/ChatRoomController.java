7e0c3cc5766979628f4de9d3bab8288ce80f8b0c
gxdxx-new <nkd0310@naver.com>
2023-06-15 00:43:28 +0900

    #59 - feat: 채팅방 API Swagger 적용

--- src/main/java/com/gxdxx/instagram/controller/ChatRoomController.java
+++ src/main/java/com/gxdxx/instagram/controller/ChatRoomController.java
@@ -2,10 +2,19 @@ package com.gxdxx.instagram.controller;
 
 import com.gxdxx.instagram.dto.request.ChatRoomListRequest;
 import com.gxdxx.instagram.dto.response.ChatRoomResponse;
+import com.gxdxx.instagram.dto.response.CommentRegisterResponse;
+import com.gxdxx.instagram.dto.response.ErrorResponse;
 import com.gxdxx.instagram.exception.InvalidRequestException;
 import com.gxdxx.instagram.service.ChatRoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+@Tag(name = "chat-rooms", description = "채팅방 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/chat-rooms")
 @RestController
@@ -21,7 +31,14 @@ public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
 
-    @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "채팅방 생성 성공",
+                    content = @Content(schema = @Schema(implementation = ChatRoomResponse.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @Operation(summary = "채팅방 생성 메소드", description = "채팅방 생성 메소드입니다.")
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ChatRoomResponse getChatRooms(
             @RequestBody @Valid ChatRoomListRequest request,
             BindingResult bindingResult,
--- src/main/java/com/gxdxx/instagram/dto/response/ChatRoomResponse.java
+++ src/main/java/com/gxdxx/instagram/dto/response/ChatRoomResponse.java
@@ -1,14 +1,17 @@
 package com.gxdxx.instagram.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record ChatRoomResponse(
 
+        @Schema(description = "다음 커서")
         @JsonProperty("next_cursor")
         Long nextCursor,
 
+        @Schema(description = "채팅방 목록")
         @JsonProperty("chat_rooms")
         List<ChatRoomListResponse> chatRooms
 
