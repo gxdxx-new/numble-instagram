ada262c559255dfa4f12195627d47c53a2fddc63
gxdxx-new <nkd0310@naver.com>
2023-06-22 23:19:58 +0900

    #61 - feat: application.yml 파일에 설정한 issuer, secret_key 값 가져오는 클래스 생성
    JWT 토큰을 만드는 데 필요한 issuer, secret_key를 변수로 접근하기 위한 클래스

---
+++ src/main/java/com/gxdxx/instagram/config/jwt/JwtProperties.java
@@ -0,0 +1,19 @@
+package com.gxdxx.instagram.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+// JWT 토큰을 만드는 데 필요
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt") // 자바 클래스에 프로퍼티값을 가져와서 사용하는 애너테이션
+public class JwtProperties {
+
+    private String issuer;
+
+    private String secretKey;
+
+}
