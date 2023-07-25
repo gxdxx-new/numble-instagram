c80139a103a7af63f82bdabdca2371ee8796606d
gxdxx-new <nkd0310@naver.com>
2023-07-25 23:55:30 +0900

    #83 - feat: 유저와 유저-역할 중간테이블 조회 쿼리 작성

--- src/main/java/com/gxdxx/instagram/domain/user/dao/UserRepository.java
+++ src/main/java/com/gxdxx/instagram/domain/user/dao/UserRepository.java
@@ -2,11 +2,18 @@ package com.gxdxx.instagram.domain.user.dao;
 
 import com.gxdxx.instagram.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByNickname(String nickname);
+
+    @Query("SELECT DISTINCT u FROM User u " +
+            "LEFT JOIN FETCH u.userRoles " +
+            "WHERE u.nickname = :nickname")
+    Optional<User> findByNicknameWithRoles(@Param("nickname") String nickname);
 
 }
