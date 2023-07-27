9573aa5d47cf0ed2384790206bd1b471f6fc3fc5
gxdxx-new <nkd0310@naver.com>
2023-07-27 13:19:56 +0900

    #85 - feat: 유저 권한 확인 테스트 컨트롤러

--- src/main/java/com/gxdxx/instagram/domain/admin/AdminController.java
+++ src/main/java/com/gxdxx/instagram/domain/admin/AdminController.java
@@ -2,7 +2,9 @@ package com.gxdxx.instagram.domain.admin;
 
 import com.gxdxx.instagram.global.common.dto.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+    @PreAuthorize("@permissionService.hasPermission('READ_USER')")
     @GetMapping
-    public SuccessResponse admin() {
+    public SuccessResponse read() {
+        return new SuccessResponse("200 SUCCESS");
+    }
+
+    @PreAuthorize("@permissionService.hasPermission('WRITE_USER')")
+    @PostMapping
+    public SuccessResponse write() {
         return new SuccessResponse("200 SUCCESS");
     }
 
