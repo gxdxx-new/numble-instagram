6ad9a436b13ca772480c00708fbf2dea56cc1ade
gxdxx-new <nkd0310@naver.com>
2023-07-31 21:19:40 +0900

    #89 - feat: 어드민 유저 리스트 조회 API Swagger 적용

--- src/main/java/com/gxdxx/instagram/domain/admin/dto/request/AdminUserQueryRequest.java
+++ src/main/java/com/gxdxx/instagram/domain/admin/dto/request/AdminUserQueryRequest.java
@@ -1,10 +1,12 @@
 package com.gxdxx.instagram.domain.admin.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record AdminUserQueryRequest(
 
+        @Schema(description = "역할명")
         @JsonProperty("role_name")
         @NotBlank
         String roleName
