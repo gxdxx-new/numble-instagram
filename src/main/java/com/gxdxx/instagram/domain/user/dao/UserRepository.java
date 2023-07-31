91d1d725a7ecf5b758d8ea8a31ca95d5981e2da9
gxdxx-new <nkd0310@naver.com>
2023-07-31 21:10:43 +0900

    #89 - feat: 어드민 유저 리스트 조회 JPQL 쿼리 작성

--- src/main/java/com/gxdxx/instagram/domain/user/dao/UserRepository.java
+++ src/main/java/com/gxdxx/instagram/domain/user/dao/UserRepository.java
@@ -1,6 +1,10 @@
 package com.gxdxx.instagram.domain.user.dao;
 
+import com.gxdxx.instagram.domain.admin.dto.response.AdminUserListResponse;
+import com.gxdxx.instagram.domain.admin.dto.response.AdminUserQueryResponse;
 import com.gxdxx.instagram.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,5 +24,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "LEFT JOIN FETCH u.userAuthorities " +
             "WHERE u.nickname = :nickname")
     Optional<User> findByNicknameWithAuthorities(@Param("nickname") String nickname);
+
+    @Query("SELECT new com.gxdxx.instagram.domain.admin.dto.response.AdminUserListResponse(u.id, u.nickname, u.profileImageUrl, u.deleted, u.createdAt, r.name) " +
+            "FROM User u " +
+            "JOIN u.userRoles ur " +
+            "JOIN ur.role r " +
+            "WHERE r.name = :roleName " +
+            "ORDER BY u.id")
+    Page<AdminUserQueryResponse> findUsersByRole(String roleName, Pageable pageable);
 
 }
