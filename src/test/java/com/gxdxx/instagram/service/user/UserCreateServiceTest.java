408d9452d74a708a282b657ae981d4a825f1b14c
gxdxx-new <nkd0310@naver.com>
2023-08-02 17:55:10 +0900

    #91 - refactor: 회원가입 Service 단위테스트 리팩토링

--- src/test/java/com/gxdxx/instagram/service/user/UserCreateServiceTest.java
+++ src/test/java/com/gxdxx/instagram/service/user/UserCreateServiceTest.java
@@ -1,11 +1,14 @@
 package com.gxdxx.instagram.service.user;
 
 import com.gxdxx.instagram.domain.user.application.UserCreateService;
+import com.gxdxx.instagram.domain.user.dao.RoleRepository;
+import com.gxdxx.instagram.domain.user.domain.Role;
 import com.gxdxx.instagram.domain.user.dto.request.UserSignUpRequest;
 import com.gxdxx.instagram.domain.user.dto.response.UserSignUpResponse;
 import com.gxdxx.instagram.domain.user.domain.User;
 import com.gxdxx.instagram.domain.user.exception.NicknameAlreadyExistsException;
 import com.gxdxx.instagram.domain.user.dao.UserRepository;
+import com.gxdxx.instagram.domain.user.exception.RoleNotFoundException;
 import com.gxdxx.instagram.global.config.s3.S3Uploader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,46 +37,64 @@ public class UserCreateServiceTest {
     @Mock
     private S3Uploader s3Uploader;
     @Mock
+    private RoleRepository roleRepository;
+    @Mock
     private PasswordEncoder passwordEncoder;
+
+    private static final String NICKNAME = "nickname";
+    private static final String EXISTING_NICKNAME = "existingNickname";
+    private static final String PASSWORD = "password";
+    private static final String ENCODED_PASSWORD = "encodedPassword";
+    private static final String STORED_FILE_URL = "storedFileUrl";
+    private static final String ROLE_NAME = "ROLE_USER";
+    private static final String ROLE_DESCRIPTION = "사용자";
+    private static final String DIR_NAME = "images";
 
     @Test
     @DisplayName("[회원가입] - 성공")
     void saveUser_withValidRequest_shouldSaveUser() {
         // given
-        String nickname = "nickname";
-        String password = "password";
-        String encodedPassword = "encodedPassword";
-        String storedFileName = "storedFileName";
-        MockMultipartFile mockFile = getMockMultipartFile();
-        UserSignUpRequest request = new UserSignUpRequest(nickname, password, mockFile);
-        when(userRepository.findByNickname(anyString())).thenReturn(Optional.empty());
-        when(s3Uploader.upload(any(), anyString())).thenReturn(storedFileName);
-        when(passwordEncoder.encode(anyString())).thenReturn(encodedPassword);
-        when(userRepository.save(any(User.class))).thenReturn(User.of(nickname, encodedPassword, storedFileName));
+        UserSignUpRequest request = createUserSignUpRequest(NICKNAME, PASSWORD);
+        Role userRole = createRole(ROLE_NAME, ROLE_DESCRIPTION);
+
+        when(userRepository.findByNickname(request.nickname()))
+                .thenReturn(Optional.empty());
+        when(s3Uploader.upload(request.profileImage(), DIR_NAME))
+                .thenReturn(STORED_FILE_URL);
+        when(passwordEncoder.encode(request.password()))
+                .thenReturn(ENCODED_PASSWORD);
+        when(userRepository.save(any(User.class)))
+                .thenReturn(User.of(request.nickname(), ENCODED_PASSWORD, STORED_FILE_URL));
+        when(roleRepository.findByName(ROLE_NAME)).thenReturn(Optional.of(userRole));
 
         // when
         UserSignUpResponse response = userCreateService.createUser(request);
 
         // then
-        assertEquals(nickname, response.nickname());
-        assertEquals(storedFileName, response.profileImageUrl());
+        assertEquals(request.nickname(), response.nickname());
+        assertEquals(STORED_FILE_URL, response.profileImageUrl());
     }
 
     @Test
     @DisplayName("[회원가입] - 실패 (이미 존재하는 닉네임)")
     void saveUser_withExistingNickname_shouldThrowException() {
         // given
-        String existingNickname = "existingNickname";
-        String password = "password";
-        String encodedPassword = "encodedPassword";
-        String storedFileName = "storedFileName";
-        MockMultipartFile mockFile = getMockMultipartFile();
-        UserSignUpRequest request = new UserSignUpRequest(existingNickname, password, mockFile);
-        User existingUser = User.of(existingNickname, encodedPassword, storedFileName);
-        when(userRepository.findByNickname(existingNickname)).thenReturn(Optional.of(existingUser));
+        UserSignUpRequest request = createUserSignUpRequest(EXISTING_NICKNAME, PASSWORD);
+        User existingUser = User.of(EXISTING_NICKNAME, ENCODED_PASSWORD, STORED_FILE_URL);
+
+        when(userRepository.findByNickname(request.nickname())).thenReturn(Optional.of(existingUser));
 
         // when & then
         assertThrows(NicknameAlreadyExistsException.class, () -> userCreateService.createUser(request));
+    }
+
+    private UserSignUpRequest createUserSignUpRequest(String nickname, String password) {
+        MockMultipartFile mockFile = getMockMultipartFile();
+        return new UserSignUpRequest(nickname, password, mockFile);
+    }
+
+    private Role createRole(String name, String description) {
+        return Role.of(name, description);
     }
 
     private MockMultipartFile getMockMultipartFile() {
